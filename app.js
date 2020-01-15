2e6c314073e1c00c8cebf293c416c163578ad4f3
heth0218 <54405324+heth0218@users.noreply.github.com>
2020-01-15 14:11:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const express=require('express');
+const app=express();
+const bodyParser=require('body-parser');
+const SpaceX=require('./mongo.js')
+const mongoose=require('mongoose');
+const key=require('./config/key')
+const fetch=require('node-fetch')
+app.listen(3000,()=>{
+    console.log("listening u on port 3000, don't worry")
+})
+app.use(express.json({limit:'50mb'}));
+app.use(express.static('public'));
+app.use(bodyParser.json())
+
+mongoose.connect(key.database)
+mongoose.connection.on('connected',()=>{
+    console.log("connected to db")
+})
+
+app.get('/api',async (req,res)=>{
+    const response=await fetch('https://api.spacexdata.com/v2/launches')
+    const json=await response.json();
+    res.send(json)
+})
+
+app.post('/api',async (req,res)=>{
+    const datas=req.body;
+    console.log(datas);
+    res.send({wish:"Congratulations, data successfully saved"})
+    datas.forEach(data => {
+        const space=new SpaceX({
+            flightNumber:data.flight_number,
+        rocketName:data.rocket.rocket_name,
+        launchDate:data.launch_year,
+        missionName:data.mission_name,
+        missionPatchLink:data.links.mission_patch
+        })
+        space.save().then(res=>console.log(res))
+    });
+   
+})
